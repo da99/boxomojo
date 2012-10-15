c8e041551448621bfa39e81d7b3257930e97a45a
da99 <i-hate-spam-45671204@mailinator.com>
2012-10-15 05:27:33 -0500

    Added: Factor_Script version of 'dup'

--- test/Core.js
+++ test/Core.js
@@ -20,5 +20,37 @@ describe( 'Core', function () {
     });
   }); // === describe
 
+  describe( '<_', function () {
+    it( 'adds a shortcut to previous item on the stack', function () {
+      assert.deepEqual( returns(' 5 <_ "five" <_ '), [ 5, 5, "five", "five" ] );
+    });
+
+    it( 'throws an error if nothing on Returns stack', function () {
+      var err = null;
+      try {
+        returns('<_');
+      } catch (e) {
+        err = e;
+      };
+      assert.equal(err.message, "<_: Returns stack can't be empty.");
+    });
+  }); // === describe
+
+  describe( '_>', function () {
+    it( 'adds a shortcut to next item on the stack', function () {
+      assert.deepEqual( returns(' _> "hi" _> 5 '), [ 'hi', 'hi', 5, 5 ] );
+    });
+
+    it( 'throws an error if nothing on Tokens stack', function () {
+      var err = null;
+      try {
+        returns('5 _>');
+      } catch (e) {
+        err = e;
+      };
+      assert.equal(err.message, "_>: Tokens stack can't be empty.");
+    });
+  }); // === describe
+
 }); // === describe
 
