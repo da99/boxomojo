6e81bd197d2fd57236f5f5b432f02cfea5f6cf64
da99 <i-hate-spam-45671204@mailinator.com>
2012-10-16 18:22:01 -0500

    Added: anonymous functions

---
+++ test/Functions.js
@@ -0,0 +1,23 @@
+
+
+var assert  = require('assert'),
+    _       = require("underscore"),
+    h       = require("factor_script/lib/test/default"),
+    Parse   = require('factor_script/lib/Parse');
+
+var new_code = h.new_code,
+    returns  = h.returns,
+    vars     = h.vars;
+
+describe( 'Functions', function () {
+
+  it( 'returns last value', function () {
+    var str = ' run { 1 + 1 + 1 } ';
+    assert.deepEqual( returns(str), [3] );
+  });
+
+  it( 'saves vars to Outside box', function () {
+    var str = ' run { "One" is: "Neo" } ';
+    assert.deepEqual( vars(str).One, "Neo" );
+  });
+}); // === describe
