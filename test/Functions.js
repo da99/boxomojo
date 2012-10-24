d977758fc515b6965454602517e4c7d1bc39ae02
da99 <i-hate-spam-45671204@mailinator.com>
2012-10-24 01:56:45 -0500

    Fixed: various bugs in running functions.

--- test/Functions.js
+++ test/Functions.js
@@ -11,13 +11,26 @@ var new_code = h.new_code,
 
 describe( 'Functions', function () {
 
-  it( 'returns last value', function () {
-    var str = ' run { 1 + 1 + 1 } ';
-    assert.deepEqual( returns(str), [3] );
-  });
+  describe( 'run: ', function () {
+    it( 'copys tokens of function', function () {
+      var code = new_code(' "F" = { 1 2 3 } run F ');
+      code.run();
+      assert.deepEqual(code.Vars['F'].Tokens, [1,2,3]);
+    });
+    it( 'does not place anything on Returns stack if function has an empty Returns stack (ie prevent undefined)', function () {
+      var str = ' run {  } ';
+      assert.deepEqual( returns(str), [ ] );
+    });
 
-  it( 'saves vars to Outside box', function () {
-    var str = ' run { "One" = "Neo" } ';
-    assert.deepEqual( vars(str).One, "Neo" );
-  });
+    it( 'returns last value', function () {
+      var str = ' run { 1 + 1 + 1 } ';
+      assert.deepEqual( returns(str), [3] );
+    });
+
+    it( 'saves vars to Outside box', function () {
+      var str = ' run { "One" = "Neo" } ';
+      assert.deepEqual( vars(str).One, "Neo" );
+    });
+  }); // === describe
+
 }); // === describe
