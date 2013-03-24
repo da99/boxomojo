e44b8884d13229c39d809e96e8eac9e4d12f05e0
da99 <i-hate-spam-1234567@mailinator.com>
2013-03-23 19:25:31 -0500

    Removed: obsolete code.

--- test/Core.js
+++ test/Core.js
@@ -18,53 +18,13 @@ describe( 'Core', function () {
     });
 
     it( 'accepts functions in Core', function () {
-      var box = new_code(' "One" = 1 [<>] get "One" ');
+      var box = new_code(' "One" = 1 [<>] : "One" ');
       box.run();
-      assert.equal( box.see_backward(), 1 );
+      assert.equal( box.read_back(), 1 );
     });
 
-    it( 'accepts functions if it\'s in the Functions list', function () {
-      var box = new_code(' [<>] yoyo ');
-      box.Vars['yoyo'] = function (box) {
-        box.respond("hiya");
-        return true;
-      };
-      box.run();
-      assert.equal( box.see_backward(), 'hiya' );
-    });
   }); // === describe
 
-  describe( '<_', function () {
-    it( 'adds a shortcut to previous item on the stack', function () {
-      assert.deepEqual( returns(' 5 <_ "five" <_ '), [ 5, 5, "five", "five" ] );
-    });
-
-    it( 'throws an error if nothing on Returns stack', function () {
-      var err = null;
-      try {
-        returns('<_');
-      } catch (e) {
-        err = e;
-      };
-      assert.equal(err.message, "<_: Returns stack can't be empty.");
-    });
-  }); // === describe
-
-  describe( '_>', function () {
-    it( 'adds a shortcut to next item on the stack', function () {
-      assert.deepEqual( returns(' _> "hi" _> 5 '), [ 'hi', 'hi', 5, 5 ] );
-    });
-
-    it( 'throws an error if nothing on Tokens stack', function () {
-      var err = null;
-      try {
-        returns('5 _>');
-      } catch (e) {
-        err = e;
-      };
-      assert.equal(err.message, "_>: Tokens stack can't be empty.");
-    });
-  }); // === describe
 
 }); // === describe
 
